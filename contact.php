e25f0316cb4c9ccd8cbbed8e10ae7fca022e00f8
lebossboss <decidaisomar@gmail.com>
2025-05-19 10:14:31 +0200

    Résolution du conflit dans contact.php

--- contact.php
+++ contact.php
@@ -6,10 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-<<<<<<< HEAD
     <p>koko sa djo fort fort mon gas 😂🫡😎 nn</p>
-=======
-    <p>koko sa djo fort fort mon gas 😂🫡😎</p>
->>>>>>> d2accd47b368b1099bafc16532745ffb0a5bc978
 </body>
 </html>
